a282acc430ba0422f699c68cd1745c1a69f4b018
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-11 13:35:50 -0400

    commit tree 191d47848ac5bf91031dcdafb59b98bca2c2fec2

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useEffect, useState} from "react";
+
+const App = () => {
+    const [isloaded, setIsLoaded] = useState(false)
+    const [randomDogUrl, setRandomDogUrl] = useState(<></>)
+    useEffect(() => { 
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(r => r.json())
+        .then(doggo => {
+            setIsLoaded(true)
+            setRandomDogUrl(doggo.message)
+        })
+    },[])
+
+    if (!isloaded) return <p>Loading...</p>;
+    return <img src={randomDogUrl} alt ='A Random Dog'></img>
+    
+}
+
+
+export default App
